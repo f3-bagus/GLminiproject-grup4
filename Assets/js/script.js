9ee013f34d794431395178a2277aec97608f860f
Ardhilla Eka <86395605+dwlndr@users.noreply.github.com>
2024-04-04 04:40:38 +0700

    fix navbar + testi yumna

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,17 @@
-let slideIndex = 0;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.querySelectorAll("testimoni");
-  let dots = document.querySelectorAll("dot");
-  if (n > slides.length) {slideIndex = 0}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 1,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
